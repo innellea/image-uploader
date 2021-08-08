52602c86f9897a1e2ee30ed64cfd12077abf5662
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-08-08 19:15:51 +0200

    make it better

--- server.js
+++ server.js
@@ -26,9 +26,7 @@ app.use('/api/upload', require('./routes/api/upload'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static(path_join(__dirname, '../client/build')));
-
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });

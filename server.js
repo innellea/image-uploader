a88a56764541782babd3b9797ee214344a124594
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-08-08 20:13:20 +0200

    make it better

--- server.js
+++ server.js
@@ -25,13 +25,14 @@ app.use('/images', express.static(__dirname + '/images'));
 app.use('/api/upload', require('./routes/api/upload'));
 
 // Serve static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//     });
-// }
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
